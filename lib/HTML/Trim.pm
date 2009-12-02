0c82b43287f57c5aac7d63b5e124ce6659fdbff4
cho45 <cho45@lowreal.net>
2009-12-02 16:26:29 +0900

    実装

--- lib/HTML/Trim.pm
+++ lib/HTML/Trim.pm
@@ -1,8 +1,58 @@
 package HTML::Trim;
-
 use strict;
 use warnings;
 our $VERSION = '0.01';
+
+use Exporter::Lite;
+use HTML::Parser;
+
+our @EXPORT_OK = qw(htrim);
+
+sub trim ($$$) {
+	my ($str, $max, $delim) = @_;
+	my $ret   = "";
+	my $count = 0;
+	my $opened = [];
+
+	my $p; $p = HTML::Parser->new(
+		api_version => 3,
+		handlers => {
+			start     => [ sub {
+				my ($text, $tagname) = @_;
+				push @$opened, $tagname unless $tagname =~ /^(input|img|br)$/;; 
+				$ret .= $text;
+			}, "text, tagname"],
+			end       => [ sub {
+				my ($text, $tagname) = @_;
+				until (!@$opened || pop @$opened eq $tagname) { }
+				$ret .= $text;
+			}, "text, tagname"],
+			text      => [ sub {
+				my ($text) = @_;
+				$count += length $text;
+				if ($count > $max) {
+					$ret .= substr($text, 0, $max - $count - 1);
+					$p->eof; # end parse immediately
+				} else {
+					$ret .= $text;
+				}
+			}, 'dtext'],
+		}
+	);
+	$p->parse($str);
+	$p->eof;
+
+	while (my $tagname = pop @$opened) {
+		$ret .= sprintf('</%s>', $tagname);
+	}
+
+	if ($count > $max) {
+		$ret .= $delim;
+	}
+	
+	$ret;
+}
+*htrim = \&trim;
 
 1;
 __END__
---
+++ t/01_base.t
@@ -0,0 +1,64 @@
+use strict;
+use warnings;
+
+use HTML::Trim;
+
+use Test::More;
+use Test::Base -Base;
+use Test::Base::Filter;
+use Encode;
+
+filters qw/norm chomp u8/;
+
+plan tests => 1 * blocks;
+run_is input => 'expected';
+
+sub u8 {
+	decode_utf8 $_;
+}
+
+sub htrim {
+	my $str  = $_;
+	my $args = filter_arguments || '';
+	my ($max, $delim) = split /\s*,\s*/, $args;
+	HTML::Trim::trim($str, $max || 5, $delim || "...");
+}
+
+
+__END__
+===
+--- input htrim
+あああ<a href="hoge">いい</a>
+--- expected
+あああ<a href="hoge">いい</a>
+
+===
+--- input htrim
+あああ<a href="hoge">いいい</a>
+--- expected
+あああ<a href="hoge">い</a>...
+
+===
+--- input htrim
+あああ<img name="bar"/>foo
+--- expected
+あああ<img name="bar"/>f...
+
+===
+--- input htrim
+あああ<img name="bar">foo
+--- expected
+あああ<img name="bar">f...
+
+===
+--- input htrim
+<div>あああ<a href="hoge">いいい</a></div>
+--- expected
+<div>あああ<a href="hoge">い</a></div>...
+
+===
+--- input htrim
+<div>あああ<a href="hoge">いいい
+--- expected
+<div>あああ<a href="hoge">い</a></div>...
+
