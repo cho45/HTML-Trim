caf3e86c8603249e7d61c642ab6c45c064f853ae
cho45 <cho45@lowreal.net>
2009-12-02 16:00:40 +0900

    init

---
+++ lib/HTML/Trim.pm
@@ -0,0 +1,33 @@
+package HTML::Trim;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+HTML::Trim -
+
+=head1 SYNOPSIS
+
+  use HTML::Trim;
+
+=head1 DESCRIPTION
+
+HTML::Trim is
+
+=head1 AUTHOR
+
+cho45 E<lt>cho45@lowreal.netE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
